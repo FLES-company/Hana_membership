79a2b619de8bb99b05e5fc7804a7495bea799e7d
flescompany <fles@fles.co.kr>
2021-02-16 11:45:21 +0900

    [First] Setting up the development environment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hello from './Hello'
+import Test from './Test'
 
 function App() {
   return (
     <div className="App">
      <Hello />
+     <Test />
     </div>
   );
 }
---
+++ src/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Test() {
+    return (
+        <div>
+            <h1>hello react!</h1>
+        </div>
+    )
+  }
+  
+  export default Test;
+  
