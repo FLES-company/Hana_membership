6d880580e5a7ec0290eed8f9b34650499553d895
binhk1004 <binhk1004@naver.com>
2021-02-23 21:09:43 +0900

    [Add] React Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import "./App.css";
 import React from "react";
-
-import Header from "./components/Header";
-import Gnb from "./components/Gnb";
-import Footer from "./components/Footer";
-
-import Login_btn from "./components/Login_button";
-import Signup_btn from "./components/SignUp_button";
-import Social_login_btn from "./components/Social_login_button";
+import { Link, Route, BrowserRouter as Router } from "react-router-dom"
+import Home from "./Home"
 import Input_text from "./components/Input_text";
 import Find_idpw from "./Find_idpw";
 
-import img from "./img/times-light.png";
+import "./App.css";
 
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-      <Input_text />
-      
-    </div>
-  );
+    <Router>
+        <main>
+          <Route exact paht = "/" component={Home} />
+          <Route path = "/input" component ={Input_text} />
+          <Route path = "/find" component ={Find_idpw} />
+        </main>
+    </Router>
+  
+    
+  )
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+               
+               
+            </div>
+        )
+    }
+}
+
+export default Home
