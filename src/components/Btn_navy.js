7984449c45fc9bdf613560c3984553e5e5ab700b
binhk1004 <binhk1004@naver.com>
2021-03-05 13:30:10 +0900

    [Modified] Modified Btn_navy.js

--- src/components/Btn_navy.js
+++ src/components/Btn_navy.js
@@ -1,22 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useHistory } from "react";
 import "./Btn_navy.css";
 import "./Guide_ui.css";
 import { Routes, Link, Switch, Route, BrowserRouter } from "react-router-dom";
 
-
 function Btn_navy() {
-  const [value, setValue] = useState('로그인');
 
-  const onChangeValue = e => { 
-    if(document.querySelectorAll('div .Btn_navy [value="로그인"]')){
-      setValue(e.target.value = "안뇽")
+  const [value, setValue] = useState('');  
+  const path = window.location.pathname
+  const handleChange = e => { 
+    if(path == '/login'){
+      setValue(e.target.value = "로그인")
     }
-    console.log(value)
-  }
+    else if(path == '/find'){
+      setValue(e.target.value = "아이디찾기")
+    }
+    console.log('완료')
+    }
 
   return (
     <div className="Btn_navy">
-      <input className="navy" type="button" value={value} onClick={onChangeValue}/>
+      <input className="navy" type="button" value={value} onClick={handleChange}/>
     </div>
   );
 }
