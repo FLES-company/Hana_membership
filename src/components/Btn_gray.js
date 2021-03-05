d7a0893d95a930c2e202ef665ec932e842adea53
csh <csh@fles.co.kr>
2021-03-05 11:18:31 +0900

    버튼스타일 클래스네임으로 변환

--- src/components/Btn_gray.js
+++ src/components/Btn_gray.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./Btn_gray.css";
 import "./Guide_ui.css";
+import classNames from 'classnames'; 
 import { Routes, Link, Switch, Route, BrowserRouter } from "react-router-dom";
 
-function Btn_gray() {
+function Btn_gray(style) {
   return (
     <div className="Btn_gray">
-      <input className="gray" type="button" value="회원가입" />
+      <input className={classNames('Btn_gray', style)} type="button" value="회원가입" />
     </div>
   );
+}
+Btn_gray.defalultProps = {
+  style: 'border',
 }
 
 export default Btn_gray;
