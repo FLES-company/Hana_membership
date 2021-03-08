2768adef273c46b809990107ec099c51ce194446
csh <csh@fles.co.kr>
2021-03-08 16:37:32 +0900

    관심회원권 모달창 레이아웃

--- src/Interest.js
+++ src/Interest.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Interest.css";
 import icon_back from "./img/right-arrow.png";
+import icon_info from "./img/icon_info.png";
 import icon_favorites from "./img/icon_favorites.png";
+import img_favorite from "./img/img_favorite.png";
 import icon_alarm_white from "./img/icon_alarm_white.png";
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 
 function Interest() {
   return (
@@ -28,6 +31,8 @@ function Interest() {
         </div>
       </div>
       {/* back_header : e */}
+
+      {/* 본 컨텐츠 : s */}
       <div className="Interest_contents">
         <table>
           <caption hidden>관심회원권</caption>
@@ -40,7 +45,7 @@ function Interest() {
             </tr>
           </thead>
           <tbody>
-            <tr>
+            <tr id="open">
               <th className="productName">88(팔팔)</th>
               <th className="price">21,000</th>
               <th className="info_compared up">
@@ -79,6 +84,7 @@ function Interest() {
           </tbody>
         </table>
       </div>
+      {/* 본 컨텐츠 : e */}
 
       {/* 관심회원권없음 empty : s */}
       <div className="Interest_contents empty">
@@ -87,11 +93,60 @@ function Interest() {
         <form>
           <div className="input_unit input_search">
             <button className="search-btn"></button>
-            <input type="text" placeholder="검색하기" />
+            <input type="text" placeholder="회원권 검색하기" />
           </div>
         </form>
       </div>
       {/* 관심회원권없음 empty : e */}
+
+      {/* 관심회원권터치 : s ( 현재 modal_wrapper -> display:none; )*/}
+      <div className="Interest_contents modal_wrapper">
+        <div className="modal">
+          <div className="modal_favorite">
+            <img src={img_favorite} alt="등록" />
+          </div>
+          <div className="modal_tit">
+            <span className="product">88(팔팔)</span>
+            <div className="modal_icon">
+              <img src={icon_info} alt="info" />
+              <img src={icon_favorites} alt="favorites" />
+            </div>
+          </div>
+          <div className="modal_content">
+            <table>
+              <tr>
+                <th>기준가(만원)</th>
+                <th>20,000</th>
+              </tr>
+              <tr>
+                <th>고가(만원)</th>
+                <th>24,000</th>
+              </tr>
+              <tr>
+                <th>저가(만원)</th>
+                <th>19,900</th>
+              </tr>
+              <tr>
+                <th>총 거래량</th>
+                <th>320</th>
+              </tr>
+            </table>
+            <div className="btn_wrapper Btn">
+              {/* 호가,차트,매수 */}
+              <button className="w72 bgGf5" type="button">
+                호가
+              </button>
+              <button className="w72 bgGf5" type="button">
+                차트
+              </button>
+              <button className="w168 bgN" type="button">
+                매수
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* 관심회원권터치 : e */}
     </div>
   );
 }
